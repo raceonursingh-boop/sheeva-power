6cf78942947de539692752c39f85fdde91d52a6e
Race <raceonursingh@Gmail.com>
2026-07-05 22:41:59 +0530

    Temporary ₹1 payment test

--- app/data/Products.ts
+++ app/data/Products.ts
@@ -4,7 +4,7 @@ export const products: Product[] = [
   {
     id: 1,
     name: "CORE",
-    price: 699,
+    price: 9,
     material: "240 GSM Premium Cotton",
     color: "Black",
     badge: "NEW",
